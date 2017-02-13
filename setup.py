625ef744469f1272cb804339fa88e0766862507d
Gagaro <gagaro42@gmail.com>
2017-02-13 14:24:56 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='django-terralego',
+    version='0.1',
+    description='Django mixins for Terralego services.',
+    url='https://terralego.fr',
+    author='Autonomens',
+    author_email='contact@autonomens.fr',
+    license='MIT',
+    packages=['django_terralego'],
+    zip_safe=False,
+)
